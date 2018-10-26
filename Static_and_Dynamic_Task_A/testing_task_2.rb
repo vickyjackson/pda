a3528fdeb9a33a7c35785a1f08eb749b71ec8208
Vicky Jackson <vicky@digitalstuff.net>
2018-10-26 17:28:05 +0100

    Completed Task A: testing_task_2

--- Static_and_Dynamic_Task_A/testing_task_2.rb
+++ Static_and_Dynamic_Task_A/testing_task_2.rb
@@ -7,27 +7,28 @@ require_relative('card.rb')
 class CardGame
 
 
-  def checkforAce(card)
-    if card.value = 1
+  def check_for_ace(card)
+    if card.value == 1
       return true
     else
       return false
     end
   end
 
-  dif highest_card(card1 card2)
-  if card1.value > card2.value
-    return card.name
-  else
-    card2
+  def highest_card(card1, card2)
+    if card1.value > card2.value
+      return card1
+    else
+      return card2
+    end
   end
-end
-end
 
-def self.cards_total(cards)
-  total
-  for card in cards
-    total += card.value
+  def self.cards_total(cards)
+    total = 0
+    for card in cards
+      total += card.value
+    end
     return "You have a total of" + total
   end
+
 end
